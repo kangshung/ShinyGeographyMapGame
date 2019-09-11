3f5e760448618b0f44a7407120bbc8933aa4cc1e
Damian Fiłonowicz <damianomancity@gmail.com>
2019-09-12 00:12:52 +0200

    Added world shapefile and basic polygon leaflet map

--- app.R
+++ app.R
@@ -1,14 +1,30 @@
 if(!require('pacman')) install.packages('pacman')
-pacman::p_load(shiny, leaflet)
+pacman::p_load(shiny, leaflet, sf)
 
 ui <- fluidPage(
-  leafletOutput('map')
+  sidebarPanel(width = 6,
+               selectInput('sf', 'Choose shapefile', 'WORLD')),
+  conditionalPanel('input.sf == "WORLD"',
+                   leafletOutput('map'),
+                   textOutput('sample'))
 )
 
 server <- function(input, output, session) {
+  select_sf <- reactive({
+    if(input$sf == 'WORLD') read_sf('ne_50m_admin_0_countries')
+  })
+  
+  sample_country <- reactive({
+    sample(select_sf()$ADMIN, 1)
+  })
+  
+  output$sample <- reactive({
+    sample_country()
+  })
+  
   output$map <- renderLeaflet({
-    leaflet() %>% 
-      addTiles()
+    leaflet(select_sf()) %>% 
+      addPolygons(color = 'gray', weight = .1, highlightOptions = highlightOptions(weight = 2))
   })
 }
 
