f620708bda3189dbf74a76d2d0a19a7860dda6a3
Damian Fiłonowicz <damianomancity@gmail.com>
2019-09-23 19:05:49 +0200

    Added map projection tests

--- app.R
+++ app.R
@@ -1,9 +1,22 @@
 if(!require('pacman')) install.packages('pacman')
 # pacman::p_load(shiny, leaflet, sf, jsonlite)
-library(shiny); library(leaflet); library(sf); library(jsonlite)
+library(shiny); library(leaflet); library(sf); library(R6); library(data.table); library(lwgeom)
 
-continents <- fromJSON('https://pkgstore.datahub.io/JohnSnowLabs/country-and-continent-codes-list/country-and-continent-codes-list-csv_json/data/c218eebbf2f8545f3db9051ac893d69c/country-and-continent-codes-list-csv_json.json')
-dplyr::left_join(read_sf('ne_10m_admin_0_countries'), continents, c(''))
+world <- setDT(st_read('ne_50m_admin_0'))
+
+map <- R6Class('map',
+  private = list(
+    option = NULL
+  ),
+  public = list(
+    sample = function() {
+      sample(world$NAME_EN)
+    }
+  )
+)
+
+
+leaflet(st_as_sf(world), options = leafletOptions(crs = leafletCRS(proj4def = '+proj=wintri'))) %>% addPolygons()
 
 ui <- bootstrapPage(theme = 'css.css',
   leafletOutput('map', height = '100vh'),
@@ -11,14 +24,12 @@ ui <- bootstrapPage(theme = 'css.css',
 )
 
 server <- function(input, output, session) {
-  select_sf <- reactive({
-    read_sf('ne_10m_admin_0_countries')
-  })
-  
   output$map <- renderLeaflet({
-    leaflet(select_sf(), options = leafletOptions(2, 5)) %>% 
+    leaflet(st_as_sf(world[CONTINENT == 'Europe']), options = leafletOptions(2, 5)) %>% 
       setView(0, 50, zoom = 2) %>% 
-      addPolygons(color = 'gray', weight = .1, highlightOptions = highlightOptions(weight = 2), layerId = ~FORMAL_EN)
+      addPolygons(fillColor = 'gray', color = 'black', weight = .1,
+                  highlightOptions = highlightOptions(weight = 2),
+                  layerId = ~NAME_EN)
   })
   
   output$text <- renderText('Click a country')
